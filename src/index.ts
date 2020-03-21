9ffd05d5c24b0a9c57f7d05b2ad0a066f1790dd8
Chris Langager <christopher.langager@gmail.com>
2020-03-21 14:50:05 -0600

    comment added to entrypoint

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,35 @@ import { logger } from './logger';
 import { CONFIG } from './config';
 import { server } from './GraphQL';
 
-(async () => {
-  logger.info({ CONFIG }, 'starting up!');
+/**
+ * The entry point is responsible for:
+ * - run database migrations (if any)
+ * - wire up an express server with middleware
+ * - bind our express server to a port and listen
+ *
+ *
+ * If you have any other special needs that need to happen at
+ * start up (ie. making an API call to some other service) it
+ * should happen here.
+ *
+ * I personally think that a more procedual style of writing code
+ * is very appropriate for entry points of a service.  Having stuff
+ * that happens when your service starts up being overly abstracted away
+ * can be very confusing to people that are new to the codebase.  I think
+ * that giving them a people a  simple starting point that they can
+ * achor the rest of the code to is important.
+ *
+ * NOTE: I'm using an immediately invoked async function here.
+ * The only purpose of this is to that I can use "await" here for db migrations.
+ * The day that top-level await is supported, this could go away.
+ */
 
-  // await migrate();
+(async () => {
+  logger.info('starting up!');
+
+  //   await migrate();
 
   const app = express();
-
   app.use(cookieParser());
   server.applyMiddleware({ app, path: '/graphql' });
 
